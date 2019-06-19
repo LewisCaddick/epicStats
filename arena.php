466329d68f6fce9026e5454df61298a5192f2f32
justcallmelewis1 <33405294+justcallmelewis1@users.noreply.github.com>
2019-06-19 16:51:23 +0100

    Cleaned Up Code
    Got the brush out as things were looking a little messy.

--- arena.php
+++ arena.php
@@ -6,36 +6,6 @@ $time  = explode(' ', $time);
 $time  = $time[1] + $time[0];
 $start = $time;
 
-?>
-<html lang="en">
- <head>
-   
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   
-  <title>Arena Seat Stats</title>
- </head>
- <body>
- <div class="container">
- </br>
-  <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-  <a class="navbar-brand" href="#">epic.28</a>
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Home</a>
-    </li>
-    <li class="nav-item active">
-      <a class="nav-link" href="arena.php">Arena</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="lakeside.php">Lakeside</a>
-    </li>
-  </ul>
-</nav>
-<?php
-
 //Curl Start
 $ch = curl_init();
 
@@ -63,37 +33,69 @@ libxml_use_internal_errors(TRUE);
 $doc->loadHTML($output);
 libxml_clear_errors(); //remove errors for yucky html
 
-$xpath = new DOMXPath($doc);
-$available  = $xpath->query('//div/div[contains(@class, "seat_available")]');
-$taken = $xpath->query('//div/div[contains(@class, "seat_taken")]');
+$xpath     = new DOMXPath($doc);
+$available = $xpath->query('//div/div[contains(@class, "seat_available")]');
+$taken     = $xpath->query('//div/div[contains(@class, "seat_taken")]');
 
 $totalAvailable = $available->length + $taken->length;
-$percentage = ($taken->length / $totalAvailable) * 100;
+$percentage     = ($taken->length / $totalAvailable) * 100;
 
-if($percentage == 100) {
-	
-	$percenttext = "100%(Full)";
+if ($percentage == 100) {
+    
+    $percenttext = "100%(Full)";
 } else {
-	
-	$percenttext = $percentage;
+    $percenttext = $percentage;
 }
 
-echo "<center><p>";
-echo "<h1><b>epic.28 Arena Seat Stats</b></h1>";
-
-echo "<h5>Total Seats: " . $totalAvailable . "</br>";
-echo "Seats Available: " . $available->length . "</br>";
-echo "Seats Taken: " . $taken->length . "</br>";
-echo "Percentage filled: " . $percenttext . "%</h5></br>";
-echo "<h5>*Please note that the total seats does not include staff seats.*</h5></br>";
-
-echo "<h5><b>Seats coming soon!</b></h5>";
-
-echo "</center>";
-
 ?>
-  </div>
- </body>
+<!doctype html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <title>Arena Seat Stats</title>
+   </head>
+   <body>
+      <div class="container">
+         </br>
+         <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+            <a class="navbar-brand" href="#">epic.28</a>
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+               </li>
+               <li class="nav-item active">
+                  <a class="nav-link" href="arena.php">Arena</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="lakeside.php">Lakeside</a>
+               </li>
+            </ul>
+         </nav>
+         <center>
+            <p>
+            <h2><b>epic.28 Arena Seat Stats</b></h2>
+            </br>
+            <div class='container'>
+               <div class='row'>
+                  <div class='col'>
+                     <h5>Total Seats: <?php echo $totalAvailable; ?></br></h5>
+                  </div>
+                  <div class='col'>
+                     <h5>Seats Available: <?php echo $available->length; ?></br></h5>
+                  </div>
+                  <div class='col'>
+                     <h5>Seats Taken: <?php echo $taken->length; ?></br></h5>
+                  </div>
+                  <div class='col'>
+                     <h5>Percentage Sold: <?php echo $percenttext; ?></p></h5>
+                  </div>
+               </div>
+            </div>
+         </center>
+      </div>
+   </body>
 </html>
 <?php
 //Page Generation End
--- lakeside.php
+++ lakeside.php
@@ -6,35 +6,6 @@ $time  = explode(' ', $time);
 $time  = $time[1] + $time[0];
 $start = $time;
 
-?>
-<html lang="en">
- <head>
-   
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   
-  <title>Lakeside Seat Stats</title>
- </head>
- <body>
- <div class="container">
- </br>
- <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
- <a class="navbar-brand" href="#">epic.28</a>
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="arena.php">Arena</a>
-    </li>
-    <li class="nav-item active">
-      <a class="nav-link" href="lakeside.php">Lakeside</a>
-    </li>
-  </ul>
-</nav>
-<?php
 //Curl Start
 $ch = curl_init();
 
@@ -62,225 +33,246 @@ libxml_use_internal_errors(TRUE);
 $doc->loadHTML($output);
 libxml_clear_errors(); //remove errors for yucky html
 
-$xpath = new DOMXPath($doc);
-$available  = $xpath->query('//div/div[contains(@class, "seat_available")]');
-$taken = $xpath->query('//div/div[contains(@class, "seat_taken")]');
+$xpath     = new DOMXPath($doc);
+$available = $xpath->query('//div/div[contains(@class, "seat_available")]');
+$taken     = $xpath->query('//div/div[contains(@class, "seat_taken")]');
 
 $totalAvailable = $available->length + $taken->length;
+$percentage     = ($taken->length / $totalAvailable) * 100;
 
-$percentage = ($taken->length / $totalAvailable) * 100;
-
-if($percentage == 100) {
-	
-	$percenttext = "100%(Full)";
+if ($percentage == 100) {
+    
+    $percenttext = "100%(Full)";
 } else {
-	
-	$percenttext = $percentage. "%";
+    $percenttext = $percentage. "%";
 }
-
-echo "<center><p>";
-echo "<h2><b>epic.28 Lakeside Seat Stats</b></h2>";
-
-echo "<h5>Total Seats: " . $totalAvailable . "</br>";
-echo "Seats Available: " . $available->length . "</br>";
-echo "Seats Taken: " . $taken->length . "</br>";
-echo "Percentage filled: " . $percenttext . "</h5></p>";
-echo "<h5>*Please note that the total seats does not include staff seats.*</h5></p>";
-
-echo "<div class='row'>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row A</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-A")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row B</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-B")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row C</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-C")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row D</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-D")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "</div>";
-
-echo "<div class='row'>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row E</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-E")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row F</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-F")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row G</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-G")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row H</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-H")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "</div>";
-
-echo "<div class='row'>";
-
-echo "<div class='col'>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row J</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-J")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-
-echo "<table class='table table-striped table-sm'>";
-echo"<thead class='thead-dark'>
-      <tr>
-        <th>Row K</th>
-      </tr>
-    </thead>
-	<tbody>
-      <tr>";
-foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-K")]') as $item) {
-    echo "<td>" .$item->nodeValue. "</td>";
-	echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-
-echo "<div class='col'>";
-echo "</div>";
-
-echo "</div>";
-
-echo "</center>";
 
 ?>
-  </div>
- </body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Lakeside Seat Stats</title>
+  </head>
+  <body>
+    <div class="container">
+      </br>
+      <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <a class="navbar-brand" href="#">epic.28</a>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="arena.php">Arena</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="lakeside.php">Lakeside</a>
+          </li>
+        </ul>
+      </nav>
+      <center>
+        <p>
+        <h2><b>Lakeside Seat Stats</b></h2>
+        </br>
+        <div class='container'>
+          <div class='row'>
+            <div class='col'>
+              <h5>Total Seats: <?php echo $totalAvailable; ?></br></h5>
+            </div>
+            <div class='col'>
+              <h5>Seats Available: <?php echo $available->length; ?></br></h5>
+            </div>
+            <div class='col'>
+              <h5>Seats Taken: <?php echo $taken->length; ?></br></h5>
+            </div>
+            <div class='col'>
+              <h5>Percentage Sold: <?php echo $percenttext; ?></p></h5>
+            </div>
+          </div>
+        </div>
+      </center>
+    </div>
+	<p>
+    <div class='container'>
+      <div class='row'>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row A</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-A")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row B</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-B")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row C</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-C")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row D</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-D")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row E</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-E")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row F</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-F")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row G</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-G")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row H</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-H")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row J</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-J")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+        <div class='col'>
+          <table class='table table-striped table-sm'>
+            <thead class='thead-dark'>
+              <tr>
+                <th>Row K</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php
+                  foreach ($xpath->query('//div[@class="seat_label" and contains(*, "LK-K")]') as $item) {
+                      echo "<td>" .$item->nodeValue. "</td>";
+                  echo "</tr>";
+                  	}
+                  	  ?>
+          </table>
+        </div>
+      </div>
+      </center>
+    </div>
+  </body>
 </html>
 <?php
 //Page Generation End
